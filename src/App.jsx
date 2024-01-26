307e8895a9f88596b1b08f1e9b755750b25eb24a
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-01-26 14:41:29 +0545

    feat: Initialize project with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className=" ">
+        <h2 className=" text-2xl font-bold ">Welcome to Arcade</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;

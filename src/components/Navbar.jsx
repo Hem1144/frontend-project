e45e32fac92af060afe24fd9fa73b942409e2afd
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-01-26 23:46:10 +0545

    feat: Update dependencies and add react-icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,42 @@
 import { Link } from "react-router-dom";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 function Navbar() {
   return (
-    <nav className=" mt-8">
-      <div className="container mx flex justify-around items-center">
-        <Link to="/" className=" text-[#111926] text-lg">
-          <span className=" text-[1.6rem] font-bold">ARCADE</span>
-        </Link>
-        <div className="flex space-x-12 ">
-          <Link to="/product" className="text-[#111926]">
-            <span className=" text-[1.2rem]">Product</span>
+    <div>
+      <nav className="container mx-auto py-6 px-36 ">
+        <div className=" flex items-center gap-36 ">
+          <Link to="/" className=" text-[#111926] text-lg">
+            <span className=" text-[1.6rem] font-bold">ARCADE</span>
           </Link>
-          <Link to="/customers" className="text-[#111926]">
-            <span className=" text-[1.2rem]">Customers</span>
-          </Link>
-          <Link to="/resources" className="text-[#111926]">
-            <span className=" text-[1.2rem]">Resources</span>
-          </Link>
-          <Link to="/company" className="text-[#111926]">
-            <span className=" text-[1.2rem]">Company</span>
-          </Link>
-          <Link to="/pricing" className="text-[#111926]">
-            <span className=" text-[1.2rem]">Pricing</span>
-          </Link>
+          <div className="flex space-x-12 ">
+            <Link to="/product" className="text-[#111926]">
+              <span className=" text-[1.2rem] flex items-center">
+                Product {<MdKeyboardArrowDown className=" text-2xl" />}
+              </span>
+            </Link>
+            <Link to="/customers" className="text-[#111926]">
+              <span className=" text-[1.2rem] flex items-center">
+                Customers {<MdKeyboardArrowDown className=" text-2xl" />}
+              </span>
+            </Link>
+            <Link to="/resources" className="text-[#111926]">
+              <span className=" text-[1.2rem] flex items-center">
+                Resources {<MdKeyboardArrowDown className=" text-2xl" />}
+              </span>
+            </Link>
+            <Link to="/company" className="text-[#111926]">
+              <span className=" text-[1.2rem] flex items-center">
+                Company {<MdKeyboardArrowDown className=" text-2xl" />}
+              </span>
+            </Link>
+            <Link to="/pricing" className="text-[#111926]">
+              <span className=" text-[1.2rem] flex items-center">Pricing</span>
+            </Link>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
 export default Navbar;
